43e98a9d9a93e692c6c007b98b4f74f66d71f43f
Esther Liu <yl3374@nyu.edu>
2020-04-09 09:18:15 -0700

    note to change phonenum validator later

--- app/classes.py
+++ app/classes.py
@@ -213,6 +213,7 @@ class RegistrationForm(FlaskForm):
     first_name = StringField("First Name:", validators=[DataRequired()])
     last_name = StringField("Last Name:", validators=[DataRequired()])
     email = StringField("Email Address:", validators=[DataRequired()])
+    # change validator to make sure phone num is 10 digits long
     phone = StringField("Phone Number:", validators=[DataRequired()])
     password = PasswordField("Create a Password:", validators=[DataRequired()])
     submit = SubmitField("Submit")
